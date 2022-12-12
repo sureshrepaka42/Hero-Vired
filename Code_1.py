17764d7419b80b3d27762b85325c49dc7c84905e
sureshrepaka42 <89093234+sureshrepaka42@users.noreply.github.com>
2022-12-12 13:38:03 +0530

    Update Code_1.py

--- Code_1.py
+++ Code_1.py
@@ -1,16 +1,16 @@
 x=open("code1_result.txt","a")
-i1=int(input("Enter First number:"))
-i2=int(input("Enter Second number:"))
-i3=int(input("Enter Third number:"))
-i4=int(input("Enter Fourth number:"))
-i5=int(input("Enter fifth number:"))
-print("The Numbers are:",i1,i2,i3,i4,i5)
-print("The Numbers are:",i1,i2,i3,i4,i5,file=x)
-if (i1<0) or (i2<0) or (i3<0) or (i4<0) or (i5<0):
+number1=int(input("Enter First number:"))
+number2=int(input("Enter Second number:"))
+number3=int(input("Enter Third number:"))
+number4=int(input("Enter Fourth number:"))
+number5=int(input("Enter fifth number:"))
+print("The Numbers are:",number1,number2,number3,number4,number5)
+print("The Numbers are:",number1,number2,number3,number4,number5,file=x)
+if (number1<0) or (number2<0) or (number3<0) or (number4<0) or (number5<0):
     print("Enter positive numbers only")
     print("Enter positive numbers only",file=x)
 else:
-    result=i1+i2+i3+i4+i5
+    result=number1+number2+number3+number4+number5
     print("Sum of numbers is:",result)
     print("Sum of numbers is:",result,file=x)
     x.close()
