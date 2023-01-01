ea08d019244b69d600f5a8ff095971245c80fecc
sureshrepaka42 <89093234+sureshrepaka42@users.noreply.github.com>
2023-01-01 14:02:38 +0530

    SQL Assignment-1

--- Q_1.sql
+++ Q_1.sql
@@ -12,16 +12,16 @@
 -- )
 /*3.Inserting values in the database*/
 -- INSERT INTO Employee_Details(Emp_ID,Emp_Name,Job_Name,Manager_ID,Hire_Date,Salary,Dep_ID)
--- VALUES(1,'Santosh','Data Scientist',100,'2022-08-21',250000,10),
--- (2,'Mahesh','Data Analyst',101,'2022-07-29',200000,11),
--- (3,'Sai','Data Engineer',102,'2022-01-13',250000,12),
--- (4,'Sankar','ML Engineer',103,'2022-07-18',300000,13),
--- (5,'Amar','Software Developer',104,'2022-12-5',100000,14),
--- (6,'Vikram','Software Engineer',105,'2022-08-29',300000,15),
--- (7,'Ram','Database Engineer',106,'2022-02-02',200000,16),
--- (8,'Rocky','Technical Associate',107,'2022-01-11',150000,17),
--- (9,'Charan','Team Lead',108,'2022-05-01',120000,18),
--- (10,'Suresh','Data Scientist',109,'2022-02-05',250000,19)
+-- VALUES(1,'Santosh','Data Scientist',1000,'2022-08-21',250000,10),
+-- (2,'Mahesh','Data Analyst',1001,'2022-07-29',200000,11),
+-- (3,'Sai','Data Engineer',1002,'2022-01-13',250000,12),
+-- (4,'Sankar','ML Engineer',1003,'2022-07-18',300000,13),
+-- (5,'Amar','Software Developer',1004,'2022-12-5',100000,14),
+-- (6,'Vikram','Software Engineer',1005,'2022-08-29',300000,15),
+-- (7,'Ram','Database Engineer',1006,'2022-02-02',200000,16),
+-- (8,'Rocky','Technical Associate',1007,'2022-01-11',150000,17),
+-- (9,'Charan','Team Lead',1008,'2022-05-01',120000,18),
+-- (10,'Suresh','Data Scientist',1009,'2022-02-05',250000,19)
 -- SELECT * FROM Employee_Details
 /*4. Getting Salaries of all the employees*/
 -- SELECT Salary from Employee_Details
