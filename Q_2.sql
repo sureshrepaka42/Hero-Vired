51979ac2d9aefd04817693ed051788a55f1864e6
sureshrepaka42 <89093234+sureshrepaka42@users.noreply.github.com>
2023-01-01 13:57:26 +0530

    SQL Assignment-1

--- Q_2.sql
+++ Q_2.sql
@@ -9,16 +9,16 @@
 -- )
 /*3.Inserting values in the table*/
 -- INSERT INTO Football_Venue(Venue_ID,Venue_Name,City_ID,Capacity)
--- VALUES(1,'California',100,50000),
--- (2,'Melbourne',101,70000),
--- (3,'Sydney',102,40000),
--- (4,'Delhi',103,60000),
--- (5,'Mumbai',104,65000),
--- (6,'Abudhabi',105,55000),
--- (7,'Chicago',106,45000),
--- (8,'Paris',107,50000),
--- (9,'Berlin',108,35000),
--- (10,'London',109,60000)
+-- VALUES(1,'USA',1000,50000),
+-- (2,'Sydney',1001,70000),
+-- (3,'Australia',1002,40000),
+-- (4,'Africa',1003,60000),
+-- (5,'India',1004,65000),
+-- (6,'Canada',1005,55000),
+-- (7,'Chicago',1006,45000),
+-- (8,'London',1007,50000),
+-- (9,'Paris',1008,35000),
+-- (10,'Canada',1009,60000)
 /*4.Count the number of venues of the football world cup*/
 --SELECT COUNT(DISTINCT Venue_Name) from Football_venue
 /*5.List all the venue names and capacities in the format of “Location” and “Volume”.*/
